bcb3576d0330e0371d1f8dbc848acdf5b5898557
YamWillie <33632555+YamWillie@users.noreply.github.com>
2021-12-15 15:32:11 -0500

    Initial Commit

---
+++ Instagram-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instagram-app
+//
+//  Created by Willie Yam on 2021-12-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
